dad066e40d21276f08cf296de67050d579d4eaac
Pablo Velez <pvelez@lean-tech.io>
2022-08-06 09:28:16 -0500

    minor changeees

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
+  
   if (isLoading) {
     return <LoadingSpinner />
   }
@@ -21,6 +22,7 @@ const App = () => {
   if (error) {
     return <ErrorMessage error={error} />
   }
+  
   return (
     <Router>
       <Switch>
